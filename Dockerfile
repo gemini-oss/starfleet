d616599c043f6c63fe04cf934c3d293c3934bd23
Mike Grima <michael.grima@gemini.com>
2023-11-20 16:42:02 -0500

    Made some small updates
    - Updated some dependency versions to hopefully
      resolve a conflict with iambic
    - Fixed iambic unit tests
    - Included a sample Dockerfile for ECR Lambda deployments

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Sample Starfleet Dockerfile for building ECR Lambda functions
+FROM public.ecr.aws/lambda/python:3.10
+
+ENV LAMBDA_TASK_ROOT=/var/runtime
+
+# Copy Starfleet over (just a quick and dirty sample, not the cleanest):
+COPY ./ ${LAMBDA_TASK_ROOT}/starfleet
+
+# Install the specified packages:
+RUN cd ${LAMBDA_TASK_ROOT}/starfleet && \
+  pip install . && \
+  rm -Rf ${LAMBDA_TASK_ROOT}/boto*  # Remove the lambda provided boto since it interferes with starfleet
+
+# The CMD is passed in as ImageConfig in the SAM template
--- tests/starfleet_included_plugins/iam/conftest.py
+++ tests/starfleet_included_plugins/iam/conftest.py
@@ -58,7 +58,7 @@ def template() -> Dict[str, Any]:
             Value: some_other_value
         IambicRoleTemplate:
           properties:
-            description: 'Starfleet iambic test role with variable {{ var.some_key }}'
+            description: 'Starfleet iambic test role with variable {{var.some_key}}'
             assume_role_policy_document:
               statement:
                 - action: sts:AssumeRole
@@ -69,7 +69,7 @@ def template() -> Dict[str, Any]:
             managed_policies:
               - policy_arn: arn:aws:iam::aws:policy/ReadOnlyAccess
             inline_policies:
-              - policy_name: 'SomePolicyIn-{{ var.account_name }}'
+              - policy_name: 'SomePolicyIn-{{var.account_name}}'
                 StarfleetIncludeAccounts:
                   ByOrgUnits:
                     - SomeNestedOU
@@ -86,6 +86,6 @@ def template() -> Dict[str, Any]:
               - key: owner
                 value: pewpewpew
               - key: some_other_key
-                value: '{{ var.some_other_key }}'
+                value: '{{var.some_other_key}}'
     """
     )
